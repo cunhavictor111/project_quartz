5c51a03f4772b95325df98d694276ff05e061469
Gustavo Nam <gustavosmn@al.insper.edu.br>
2022-11-27 17:20:09 -0300

    animação_V3

--- código_movimento.py
+++ código_movimento.py
@@ -20,17 +20,31 @@ player_idle_images = [pygame.image.load("Individual Sprites/adventurer-idle-00.p
                       pygame.image.load('Individual Sprites/adventurer-idle-02.png'),
                       pygame.image.load('Individual Sprites/adventurer-idle-03.png')]
 
+player_weapon = pygame.image.load("Kar98k.png").convert()
+player_weapon.set_colorkey((0, 0, 0))
 
 class Player:
     def __init__(self, x, y, width, height):
-        self.x = x
-        self.y = y
+        self.x = x - 30
+        self.y = y - 30
         self.width = width
         self.height = height
         self.animation_count = 0
         self.moving_right = False
         self.moving_left = False
         self.moving_vertical = False
+
+    def handle_weapons(self, display):
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+
+        rel_x, rel_y = mouse_x - player.x, mouse_y - player.y
+        angle = (100/math.pi) * -math.atan2(rel_y, rel_x)
+
+        player_weapon_copy = pygame.transform.rotate(player_weapon, angle)
+
+        display.blit(player_weapon_copy, (self.x + 30 - int(player_weapon_copy.get_width()/2),
+                                          self.y + 40 - int(player_weapon_copy.get_height()/2)))
+
 
     def main(self, display):
         if self.animation_count + 1 >= 16:
@@ -41,15 +55,16 @@ class Player:
             display.blit(pygame.transform.scale(
                 pygame.transform.flip(player_walk_images[self.animation_count // 4], True, False), (64, 84)),
                          (self.x, self.y))
+        elif self.moving_left and self.moving_vertical:
+            display.blit(pygame.transform.scale(player_walk_images[self.animation_count//4], (64, 84)), (self.x, self.y))
         elif self.moving_vertical:
-            display.blit(pygame.transform.scale(
-                pygame.transform.flip(player_walk_images[self.animation_count // 4], True, False), (64, 84)),
-                (self.x, self.y))
+            display.blit(pygame.transform.scale(player_walk_images[self.animation_count//4], (64, 84)), (self.x, self.y))
         elif self.moving_left:
             display.blit(pygame.transform.scale(player_walk_images[self.animation_count//4], (64, 84)), (self.x, self.y))
         else:
             display.blit(pygame.transform.scale(player_idle_images[self.animation_count//4], (64, 84)), (self.x, self.y))
 
+        self.handle_weapons(display)
 
         #pygame.draw.rect(display, (255, 0, 0), (self.x, self.y, self.width, self.height))
         self.moving_right = False
@@ -59,8 +74,8 @@ class Player:
 
 class PlayerBullet:
     def __init__(self, x, y, mouse_x, mouse_y):
-        self.x = x
-        self.y = y
+        self.x = x + 30
+        self.y = y + 30
         self.width = mouse_x
         self.height = mouse_y
         self.speed = 15
